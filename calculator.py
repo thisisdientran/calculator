41ae93df6d3939a962d2280ffd5c11800f042390
Dien Tran <59813041+tuandien1998@users.noreply.github.com>
2023-11-21 23:41:15 -0800

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -21,5 +21,7 @@ elif choice == 2:
     print(subtract(num1, num2))
 elif choice == 3:
     print(multiply(num1, num2))
-else:
+elif choice == 4:
     print(divide(num1, num2))
+else:
+    print("Please choice 1 to 4")
